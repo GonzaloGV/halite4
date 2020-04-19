42ddd97af3283f0b7d89a32b79bd3d037304a6ba
gonzalo <gonzalovasquezher@gmail.com>
2020-04-18 22:32:09 -0300

    corregi run_environment y ahora devuelve un html

--- run_environment.py
+++ run_environment.py
@@ -3,4 +3,5 @@ from kaggle_environments import make, evaluate
 env = make('halite', debug=True)
 
 env.run(['bot.py', 'bot.py'])
-env.render(mode='ipython', width=400, height=300)
+with open('run.html', 'w') as file:
+    file.write(env.render(mode='html', width=400, height=300))
